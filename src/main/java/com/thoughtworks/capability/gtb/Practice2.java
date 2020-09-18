90ece69b0ac4fcdea2ef23053df8203f24d84896
ClarkTheSup <lknkoqq123@163.com>
2020-09-18 14:50:53 +0800

    [kangning] feat: finish practice2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 /**
@@ -11,6 +12,12 @@ import java.time.LocalDate;
 public class Practice2 {
 
   public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
+    DayOfWeek dayOfWeek = date.getDayOfWeek();
+    int dayOfWeekInt = dayOfWeek.getValue();
+    if (dayOfWeekInt >=1 && dayOfWeekInt <=4) {
+      return date.plusDays(1);
+    } else {
+      return date.plusDays(8 - dayOfWeekInt);
+    }
   }
 }
